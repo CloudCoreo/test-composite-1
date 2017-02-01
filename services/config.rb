5e5ed95fbba34b11634d7cf8ed082ef67fa7eb66
George Moberly <george.moberly@gmail.com>
2017-02-01 11:41:58 -0800

    a

--- services/config.rb
+++ services/config.rb
@@ -71,4 +71,90 @@ coreo_uni_util_variables "resource_10" do
                 {'COMPOSITE::coreo_uni_util_variables.resource_10.var_10' => '10'}
             ])
 end
-
+coreo_uni_util_variables "resource_11" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.resource_11.var_11' => '11'}
+            ])
+end
+coreo_uni_util_variables "resource_12" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.resource_12.var_12' => '12'}
+            ])
+end
+coreo_uni_util_variables "resource_13" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.resource_13.var_13' => '13'}
+            ])
+end
+coreo_uni_util_variables "resource_14" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.resource_14.var_14' => '14'}
+            ])
+end
+coreo_uni_util_variables "resource_15" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.resource_15.var_15' => '15'}
+            ])
+end
+coreo_uni_util_variables "resource_16" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.resource_16.var_16' => '16'}
+            ])
+end
+coreo_uni_util_variables "resource_17" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.resource_17.var_17' => '17'}
+            ])
+end
+coreo_uni_util_variables "resource_18" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.resource_18.var_18' => '18'}
+            ])
+end
+coreo_uni_util_variables "resource_19" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_uni_util_variables.resource_19.var_19' => '19'}
+            ])
+end
+coreo_uni_util_variables "resource_20" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_21" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_22" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_23" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_24" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_25" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_26" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_27" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_28" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_29" do
+  action :nothing
+end
+coreo_uni_util_variables "resource_30" do
+  action :nothing
+end
